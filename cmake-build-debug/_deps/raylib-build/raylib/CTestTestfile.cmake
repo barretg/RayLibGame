344d0ed04b815afa26507241d71faec268685e88
Barret Glass <89748107+barretg@users.noreply.github.com>
2023-10-11 13:47:33 -0500

    Delete cmake-build-debug directory

--- cmake-build-debug/_deps/raylib-build/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/barretglass/CLionProjects/RayLibGame/cmake-build-debug/_deps/raylib-src/src
-# Build directory: /Users/barretglass/CLionProjects/RayLibGame/cmake-build-debug/_deps/raylib-build/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external/glfw")
